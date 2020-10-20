ad2230ddf8eb3c65ab80443d1b5b44e016ecd25a
Casper Feng <casp.feng@gmail.com>
2020-10-20 20:51:02 +0200

    add examplecomponent

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ function App() {
   return (
     <>
       <h1>Velkommen til JrC React-TS Baseline!</h1>
+      <ul>
+        <li>Les README for informasjon om bruk</li>
+      </ul>
     </>
   );
 }
--- src/pages/ExamplePage.tsx
+++ src/pages/ExamplePage.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import ExampleComponent from '../components/ExampleComponent';
 
 interface ExamplePageProps {}
 
 const ExamplePage: React.FC<ExamplePageProps> = () => {
-  return <div><h1>Velkommen til en eksempelside</h1></div>;
+  return <div><h1>Velkommen til en eksempelside</h1>
+  <ExampleComponent />
+  </div>;
 };
 
 export default ExamplePage;
