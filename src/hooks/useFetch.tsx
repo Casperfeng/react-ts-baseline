e14b04991ffb77e5b06216969604dfbf511d4854
Casper Feng <casp.feng@gmail.com>
2020-10-20 21:22:14 +0200

    add loading indicator to useFetch

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -4,16 +4,19 @@ import axios from 'axios';
 export const useFetch = (url: string): any => {
   const [response, setResponse] = useState<any>(null);
   const [error, setError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
     const fetchData = async () => {
+      setIsLoading(true);
       try {
         const res = await axios.get(url);
         setResponse(res.data);
+        setIsLoading(false);
       } catch (error) {
         setError(error);
       }
     };
     fetchData();
   }, [url]);
-  return { response, error };
+  return { response, isLoading, error };
 };
